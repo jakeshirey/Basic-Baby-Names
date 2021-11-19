c0086a7f713becca7bbb526aff0aada186282f38
jakeshirey <94328784+jakeshirey@users.noreply.github.com>
2021-11-19 14:17:52 -0500

    Update graph-backend.cpp

--- graph-backend.cpp
+++ graph-backend.cpp
@@ -5,32 +5,48 @@ using namespace std;
 
 class User
 {
+	static int numUsers;
 	string name;
 	string userID;
 	unsigned int age;
 
 	//====Creation and Destruction====
 	User();
-	User(string name, string age);
+	User(string _name, int _age);
+	static string makeID(int num);
 };
 class Graph
 {
 	unsigned int numVertices;
-	unordered_map<string, unsigned int> vertLookup; //matches a string ID (obj attribute) to a vertex index
-	unordered_map<unsigned int, vector<int>> adjList; //connects vertices to other vertices
-	unordered_map<unsigned int, User*> userLookup; //match a vertex to a person object
+	unordered_map<User, vector<User>> adjList;
 
-	/* im thinking like userLookup[adjList[vertLookup["bobert"]].at(0)]
-	*  Tracks from name to index to neighbor index to user 
-	* */
 
 	//====Creation and Destruction====
 	Graph();
 	~Graph();
 	//====Getters and Setters====
-	unsigned int numVertices();
+	unsigned int numVertices() { return numVertices; };
 	//====Insertion and Deletion====
 
 	//====Standard Algorithms====
 
 };
+
+//==================CREATION AND DESTRUCTION=================================
+User::User()
+{
+	name = "";
+	age = -1;
+	userID = makeID(++numUsers);
+}
+User::User(string _name, int _age)
+{
+	name = _name;
+	age = _age;
+	userID = makeID(++numUsers);
+}
+string User::makeID(int num)
+{
+	return to_string(num); //Will probably be changed later based on needs
+}
+int User::numUsers = 0;
